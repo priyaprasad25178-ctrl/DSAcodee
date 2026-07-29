bae69ec6472027d9214b3d5e3cf0ad6868ea32bd
Priya Prasad <priyaprasad25178@gmail.com>
2026-07-29 16:23:46 +0530

    Time: 3 ms (44.49%), Space: 8.7 MB (37.62%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int temp = x;
+        long long rev = 0;
+
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        return rev == x;
+    }
+};
